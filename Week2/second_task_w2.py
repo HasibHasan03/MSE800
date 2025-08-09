89a01a1f4f976664e0c64140778a2df84e9421a3
HasibHasan03 <hasibhasan03@gmail.com>
2025-08-10 10:06:35 +1200

    add new func for word count

--- Week2/second_task_w2.py
+++ Week2/second_task_w2.py
@@ -13,14 +13,18 @@ class StringManipulator:
 
     def to_lowercase(self):
         return self.text.lower()
+    
+    def word_count(self):
+        return len(self.text.split())
 
 def main():
     # Emam usage of the StringManipulator class and its methods
-    manipulator = StringManipulator("Emam")
+    manipulator = StringManipulator("Emam is learning Python")
     print(f"Index of 'm' {manipulator.find_char('a')}")  
     print(f"Length of string: {manipulator.get_length()}")     
     print(f"Uppercase: {manipulator.to_uppercase()}")   
-    print(f"Lowercase: {manipulator.to_lowercase()}")   
+    print(f"Lowercase: {manipulator.to_lowercase()}")  
+    print(f"Word count: {manipulator.word_count()}") 
 
 if __name__ == "__main__":
     main()
