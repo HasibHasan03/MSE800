a8b3ba1bfc72866b72119c753cfdbf4867d2556a
HasibHasan03 <hasibhasan03@gmail.com>
2025-08-10 09:50:48 +1200

    Not init main function

---
+++ Week2/second_task_w2 _main_not_ini copy.py
@@ -0,0 +1,31 @@
+class StringManipulator:
+    def __init__(self, text):
+        self.text = text
+    
+    
+    def find_char(self, char):
+        return self.text.find(char)
+
+    def get_length(self):
+        return len(self.text)
+    
+    def to_uppercase(self):
+        return self.text.upper()
+
+    def to_lowercase(self):
+        return self.text.lower()
+
+def main():
+    # Emam usage of the StringManipulator class and its methods
+    manipulator = StringManipulator("Emam")
+    print(f"Index of 'm' {manipulator.find_char('a')}")  
+    print(f"Length of string: {manipulator.get_length()}")     
+    print(f"Uppercase: {manipulator.to_uppercase()}")   
+    print(f"Lowercase: {manipulator.to_lowercase()}")   
+
+# if __name__ == "__main__":
+#     main()
+## 
+# if main is not called, the code will not execute when imported as a module.
+# TypeError: StringManipulator() takes no arguments
+##
