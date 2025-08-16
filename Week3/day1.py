21b975ec6b7d2fccb2f9a04aeb4e98188002bc33
HasibHasan03 <hasibhasan03@gmail.com>
2025-08-16 12:49:00 +1200

    up File

--- Week3/day1.py
+++ Week3/day1.py
@@ -15,7 +15,7 @@ class FileReader:
             print(f"Error: File '{self.filename}' not found.")
         except UnicodeDecodeError:
             print(f"Error: Cannot decode file '{self.filename}'. Check encoding.")
-
+    # thsis method reads the file and stores each line in self.data at the last index
     def append_to_file(self, text):
         """Append a new line to the file"""
         try:
@@ -25,11 +25,17 @@ class FileReader:
         except Exception as e:
             print(f"Error writing to file: {e}")
 
+    # task 2: to count the total words in the file
+    def count_words(self):
+        """Count total words in the file"""
+        total_words = sum(len(line.split()) for line in self.data)
+        print(f"Total words in the file: {total_words}")
+        return total_words
+    
     def display_data(self):
         """Print the data"""
         for line in self.data:
-            print(line)
-
+            print(line)  
 
 # --- Using the class ---
 os.chdir("E:/Applications/Python/MSE800/Week3")
@@ -37,9 +43,9 @@ os.chdir("E:/Applications/Python/MSE800/Week3")
 reader = FileReader("demo.txt")
 # reader = FileReader("E:/Applications/Python/MSE800/Week3/demo.txt")  # create object
 
-
 # Append new lines
 reader.append_to_file("This new line: Emam Hasan")
 
 reader.read_file()               # read the file
 reader.display_data()            # show the content
+reader.count_words()             # count words in the file
